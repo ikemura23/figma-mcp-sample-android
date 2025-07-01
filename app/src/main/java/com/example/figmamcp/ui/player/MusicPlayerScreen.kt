5c59d726d820297f2ba196fba1df57529f27da34
Kazutaka Ikemura <banbara.work@gmail.com>
2025-07-01 10:11:25 +0900

    feat: add MusicPlayerScreen composable with preview

---
+++ app/src/main/java/com/example/figmamcp/ui/player/MusicPlayerScreen.kt
@@ -0,0 +1,22 @@
+package com.example.figmamcp.ui.player
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.figmamcp.ui.theme.FigmamcpsampleandroidTheme
+
+@Composable
+fun MusicPlayerScreen(modifier: Modifier = Modifier) {
+    Column(modifier) {
+        // TODO ここに各コンポーネントを配置する
+    }
+}
+
+@Preview
+@Composable
+private fun MusicPlayerScreenPreview() {
+    FigmamcpsampleandroidTheme {
+        MusicPlayerScreen()
+    }
+}
